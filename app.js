76f113877da0541f62eb13f825e6d8a3bfee8546
souravbsk <souravbsk01@gmail.com>
2022-08-08 17:25:33 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const faqBtn = document.getElementById("faq");
+const faqAns = document.getElementById("faq-ans");
+faqBtn.addEventListener("click", () => {
+	if (faqAns.className === "hidden") {
+		faqAns.classList.remove("hidden");
+	}
+	else {
+		faqAns.classList.add("hidden");
+	}
+})
+const faqBtn2 = document.getElementById("faq2");
+const faqAns2 = document.getElementById("faq-ans2");
+faqBtn2.addEventListener("click", () => {
+	if (faqAns2.className === "hidden") {
+		faqAns2.classList.remove("hidden");
+	}
+	else {
+		faqAns2.classList.add("hidden");
+	}
+})
+const faqBtn3 = document.getElementById("faq3");
+const faqAns3 = document.getElementById("faq-ans3");
+faqBtn3.addEventListener("click", () => {
+	if (faqAns3.className === "hidden") {
+		faqAns3.classList.remove("hidden");
+	}
+	else {
+		faqAns3.classList.add("hidden");
+	}
+})
+const faqBtn4 = document.getElementById("faq4");
+const faqAns4 = document.getElementById("faq-ans4");
+faqBtn4.addEventListener("click", () => {
+	if (faqAns4.className === "hidden") {
+		faqAns4.classList.remove("hidden");
+	}
+	else {
+		faqAns4.classList.add("hidden");
+	}
+})
+
+const faqBtn5 = document.getElementById("faq5");
+const faqAns5 = document.getElementById("faq-ans5");
+faqBtn5.addEventListener("click", () => {
+	if (faqAns5.className === "hidden") {
+		faqAns5.classList.remove("hidden");
+	}
+	else {
+		faqAns5.classList.add("hidden");
+	}
+})
